cf65a94f45c94e14011d81f0a7c3ad4d99add753
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2024-07-27 02:45:08 +0700

    Update searchp.js

--- searchp.js
+++ searchp.js
@@ -369,7 +369,7 @@
         }
 
         if (_show()) {
-            load(blogger('298900102869691923') + param(extend(settings.query, {
+            load(blogger('3790642101529875438') + param(extend(settings.query, {
                 'callback': '_' + fn + '_',
                 'max-results': 21,
                 'orderby': 'updated'
